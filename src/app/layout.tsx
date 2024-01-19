de9c9722035e6ed8c21a1398433995ba96478ccc
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-01-19 22:46:16 +0100

    I added main css classname to children of the main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           <AdminBar />
           {/* @ts-expect-error */}
           <Header />
-          <main>
+          <main className="main">
             {children}
           </main>
           {/* @ts-expect-error */}
