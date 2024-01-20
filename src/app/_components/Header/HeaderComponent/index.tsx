9980b69df6fa2c01d0715b955e81387461c309be
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-01-20 13:44:35 +0100

    I display nav only on the proper pages

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,22 +1,30 @@
+'use client' //you must use this directive when you wanna use 'pathname'
+
 import React from 'react'
-import { Header } from '../../../../payload/payload-types'
-import { Gutter } from '../../Gutter'
-import  Link  from 'next/link'
 import Image from 'next/image'
+import  Link  from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { Header } from '../../../../payload/payload-types'
+import { noHeaderFooterUrls } from '../../../constants'
+import { Gutter } from '../../Gutter'
+import { HeaderNav } from '../Nav'
 
 import classes from './index.module.scss'
-import { HeaderNav } from '../Nav'
-import MobileNav from '../MobileNav'
 
 const HeaderComponent = ({ header }: { header: Header}) => {
+  const pathname = usePathname()
+
   return (
-    <nav className={classes.header}>
+    <nav //add class hide when noHeaderFooterUrls includes proper pathname
+        className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide] 
+        .filter(Boolean) //change array string to string and join space to it
+        .join(' ')}> 
        <Gutter className={classes.wrap}>
             <Link href="/">
                 <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
             </Link>
             <HeaderNav header={header} />
-            <MobileNav header={header}/>
         </Gutter> 
     </nav>
   )
