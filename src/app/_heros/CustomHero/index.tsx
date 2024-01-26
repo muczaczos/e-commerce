e78e0a483d342cd5513ad645a22ecc12f0dc031a
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-01-26 15:03:56 +0100

    I added custom hero and fetch categories
    I added new hero type. Like always after touching type you must do this in terminal:
    yarn payload generate: graphQLSchema
    
    yarn generate:types
    
    after reloading the server you can add new custom hero from the dashboard

---
+++ src/app/_heros/CustomHero/index.tsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react'
+
+import { Page } from '../../../payload/payload-types'
+import { Gutter } from '../../_components/Gutter'
+import { CMSLink } from '../../_components/Link'
+import { Media } from '../../_components/Media'
+import RichText from '../../_components/RichText'
+
+import classes from './index.module.scss'
+
+export const CustomHero: React.FC<Page['hero']> = ({ richText, media, links }) => {
+  return (
+    <Gutter className={classes.hero}>
+      <h1>Welcome to custome dupa</h1>
+    </Gutter>
+  )
+}
