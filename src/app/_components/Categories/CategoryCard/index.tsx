3705fae66abbd12eccbb7b528fc30e893b0007b0
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-01-27 17:39:52 +0100

    I used categoryFilters in onClick event in CategoryCard

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import { Category, Media } from '../../../../payload/payload-types'
 
 import classes from './index.module.scss'
+import { useFilter } from '../../../_providers/Filter'
 
 type CategoryCardProps = {
   category: Category
@@ -12,10 +13,14 @@ type CategoryCardProps = {
 
 const CategoryCard = ({ category }: CategoryCardProps) => {
     const media = category.media as Media
+    const {setCategoryFilters} = useFilter()
 
   return (
-    <Link href="/products" className={classes.card} 
-        style={{backgroundImage: `url(${media.url})`}}
+    <Link 
+      href="/products" 
+      className={classes.card} 
+      style={{backgroundImage: `url(${media.url})`}}
+      onClick={() => setCategoryFilters([category.id])}
     >
       <p className={classes.title}>{category.title}</p>
     </Link>
