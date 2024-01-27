504a9b0ca3f26c759a0bf7640c51f65821ba2e86
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-01-27 10:12:57 +0100

    I added empty 'categories' component to homepage

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -12,6 +12,7 @@ import { Gutter } from '../../_components/Gutter'
 import { Hero } from '../../_components/Hero'
 import { generateMeta } from '../../_utilities/generateMeta'
 
+
 // Payload Cloud caches all files through Cloudflare, so we don't need Next.js to cache them as well
 // This means that we can turn off Next.js data caching and instead rely solely on the Cloudflare CDN
 // To do this, we include the `no-cache` header on the fetch requests used to get the data for this page
@@ -21,12 +22,15 @@ import { generateMeta } from '../../_utilities/generateMeta'
 export const dynamic = 'force-dynamic'
 
 import classes from '/.index.module.scss'
+import Categories from '../../_components/Categories'
+
 
 export default async function Page({ params: { slug = 'home' } }) {
   const { isEnabled: isDraftMode } = draftMode()
 
   let page: Page | null = null
-
+  let categories: Category[] | null = null
+  
   try {
     page = await fetchDoc<Page>({
       collection: 'pages',
@@ -59,8 +63,8 @@ export default async function Page({ params: { slug = 'home' } }) {
         <section>
           <Hero {...hero} />
 
-          <Gutter>
-            {/* categories */ }
+          <Gutter className='classes.home'>
+            <Categories categories={categories}/>
           </Gutter>
         </section>
       ) : (
