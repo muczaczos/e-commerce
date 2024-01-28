57a37ad11c384ccfca11dabb14c14ceed6245ee9
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-01-28 11:57:04 +0100

    I added 'StatBox' Component inside 'Promotion' Component

--- src/app/_components/Promotion/index.tsx
+++ src/app/_components/Promotion/index.tsx
@@ -10,8 +10,22 @@ export const Promotion = () => {
             Get ready for a shopping experience like never before
             with our Deals of the Month! Every purchase comes with
             exlusive perks and offers, makingn this month a
-            celebration of savvy choices and amazing deals. Don't miss out! 
+            celebration of savvy choices and amazing deals. Don't miss out! 🎁 🧺
         </p>
+
+        <ul className={classes.stats}>
+          <StatBox label="Days" value={2}/>
+        </ul>
     </div>
   </section>
 }
+
+const StatBox = ({ label, value }: {label: string; value: number }) => (
+  <li className={classes.statBox}>
+    <h4>{ value }</h4>
+    <p>{ label }</p>
+  </li>
+)
+
+
+export default Promotion
