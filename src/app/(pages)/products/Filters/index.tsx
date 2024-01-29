b7a86a176e8137ec925d53b8c4ea30fdd7ca08cd
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-01-29 20:18:33 +0100

    I map categories in filter and added props

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -1,11 +1,34 @@
+'use client'
+
 import React from 'react'
 
 import classes from './index.module.scss'
+import { useFilter } from '../../../_providers/Filter'
+import { Category } from '../../../../payload/payload-types'
+import { Checkbox } from 'payload/components/forms'
 
-const Filters = () => {
-  return (
-    <div>Filters</div>
-  )
+const Filters = ({ categories }: { categories: Category[] }) => {
+  const { categoryFilters, sort, setCategoryFilters, setSort} =
+  useFilter()
+
+  const handleCategories = (categoryId: string) => {
+
+  }
+
+  return <div className={classes.filters}>
+    <div>
+      <h6 className={classes.title}>Product Categories</h6>
+      <div className={classes.categories}>
+        {categories.map(category => {
+          const isSelected = false
+
+          return (
+            <div></div>
+          )
+          })}
+        </div>
+      </div>
+      </div>
 }
 
 export default Filters
