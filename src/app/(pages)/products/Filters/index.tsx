66471d380675ff6e29661fb44973c5d0abbf07d3
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-01-30 14:07:30 +0100

    I added 'checkbox' component to filter

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -2,33 +2,39 @@
 
 import React from 'react'
 
-import classes from './index.module.scss'
-import { useFilter } from '../../../_providers/Filter'
 import { Category } from '../../../../payload/payload-types'
-import { Checkbox } from 'payload/components/forms'
+import { Checkbox } from '../../../_components/Checkbox'
+import { useFilter } from '../../../_providers/Filter'
+
+import classes from './index.module.scss'
 
 const Filters = ({ categories }: { categories: Category[] }) => {
-  const { categoryFilters, sort, setCategoryFilters, setSort} =
-  useFilter()
+  const { categoryFilters, sort, setCategoryFilters, setSort } = useFilter()
 
-  const handleCategories = (categoryId: string) => {
+  const handleCategories = (categoryId: string) => {}
 
-  }
+  return (
+    <div className={classes.filters}>
+      <div>
+        <h6 className={classes.title}>Product Categories</h6>
+        <div className={classes.categories}>
+          {categories.map(category => {
+            const isSelected = false
 
-  return <div className={classes.filters}>
-    <div>
-      <h6 className={classes.title}>Product Categories</h6>
-      <div className={classes.categories}>
-        {categories.map(category => {
-          const isSelected = false
-
-          return (
-            <div></div>
-          )
+            return (
+              <Checkbox
+                key={category.id}
+                label={category.title}
+                value={category.id}
+                isSelected={isSelected}
+                onClickHandler={handleCategories}
+              />
+            )
           })}
         </div>
       </div>
-      </div>
+    </div>
+  )
 }
 
 export default Filters
