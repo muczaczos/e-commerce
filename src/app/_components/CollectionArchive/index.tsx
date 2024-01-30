dc4d8df32a681b44edcaf6f5d249b2377cd1ef30
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-01-30 18:15:25 +0100

    I chenged CollectionArchive styles

--- src/app/_components/CollectionArchive/index.tsx
+++ src/app/_components/CollectionArchive/index.tsx
@@ -172,10 +172,9 @@ export const CollectionArchive: React.FC<Props> = props => {
   return (
     <div className={[classes.collectionArchive, className].filter(Boolean).join(' ')}>
       <div className={classes.scrollRef} ref={scrollRef} />
-      {!isLoading && error && <Gutter>{error}</Gutter>}
+      {!isLoading && error && <div>{error}</div>}
       <Fragment>
         {showPageRange !== false && populateBy !== 'selection' && (
-          <Gutter>
             <div className={classes.pageRange}>
               <PageRange
                 collection={relationTo}
@@ -184,17 +183,11 @@ export const CollectionArchive: React.FC<Props> = props => {
                 totalDocs={results.totalDocs}
               />
             </div>
-          </Gutter>
         )}
-        <Gutter>
           <div className={classes.grid}>
             {results.docs?.map((result, index) => {
               if (typeof result === 'object' && result !== null) {
-                return (
-                  <div className={classes.column} key={index}>
-                    <Card doc={result} relationTo={relationTo} showCategories />
-                  </div>
-                )
+                return <Card doc={result} relationTo={relationTo} showCategories />
               }
 
               return null
@@ -208,7 +201,6 @@ export const CollectionArchive: React.FC<Props> = props => {
               totalPages={results.totalPages}
             />
           )}
-        </Gutter>
       </Fragment>
     </div>
   )
