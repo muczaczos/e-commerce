074ef0b11074a308099f83511568eeb60df0c0b8
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-02-01 14:06:21 +0100

    I started creating new cart

---
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import React, { useState } from 'react'
+
+import classes from './index.module.scss'
+
+const CartItem = ({ product, title, metaImage, qty, price, addItemToCart }) => {
+  const [quantity, setQuantity] = useState(qty)
+
+  return (
+    <li className={classes.item}>
+      <h6>TITLE</h6>
+    </li>
+  )
+}
+
+export default CartItem
