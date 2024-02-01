f46f3edf07eaba171a239ec2e8de465307ba16df
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-02-01 14:08:23 +0100

    I added empty function for increment quantity

--- src/app/(pages)/cart/CartItem/index.tsx
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -7,6 +7,10 @@ import classes from './index.module.scss'
 const CartItem = ({ product, title, metaImage, qty, price, addItemToCart }) => {
   const [quantity, setQuantity] = useState(qty)
 
+  const decrementQty = () => {}
+  const incrementQty = () => {}
+  const enterQty = () => {}
+
   return (
     <li className={classes.item}>
       <h6>TITLE</h6>
