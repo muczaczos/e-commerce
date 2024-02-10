c6e28d7b3508d99693e7516471f7be02c1e912d9
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-02-10 11:37:49 +0100

    Now shipping details from form will save with order

--- src/app/(pages)/checkout/CustomCheckoutForm/index.tsx
+++ src/app/(pages)/checkout/CustomCheckoutForm/index.tsx
@@ -34,8 +34,31 @@ const CustomCheckoutForm: React.FC<{method: string}> = ({ method }) => {
   const [email, setEmail] = React.useState('')
 
   const handleName = (e) => {
-    console.log(e.target.value)
     setFullName(e.target.value)
+  }
+
+  const handleAddress = (e) => {
+    setAddress(e.target.value)
+  }
+
+  const handleCity = (e) => {
+    setCity(e.target.value)
+  }
+
+  const handlePostalCode = (e) => {
+    setPostalCode(e.target.value)
+  }
+
+  const handleCountry = (e) => {
+    setCountry(e.target.value)
+  }
+
+  const handlePhone = (e) => {
+    setPhone(e.target.value)
+  }
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
   }
 
   const handleSubmit = async () => {
@@ -116,6 +139,12 @@ const CustomCheckoutForm: React.FC<{method: string}> = ({ method }) => {
             product: typeof product === 'string' ? product : product.id,
             quantity,
             fullname: fullName,
+            streetAddress: address,
+            city: city,
+            postalCode: postalCode,
+            country: country,
+            phoneNumber: phone,
+            email: email,
             price:
               typeof product === 'object' ? priceFromJSON(product.priceJSON, 1, true) : undefined,
           })),
@@ -153,22 +182,22 @@ const CustomCheckoutForm: React.FC<{method: string}> = ({ method }) => {
         />
       </div>
       <div className={classes.address}>
-        <Input name="address" type="text" label="Street Address" register={register} error={null} />
+        <Input name="address" type="text" label="Street Address" register={register} error={null} onChange={handleAddress} />
       </div>
       <div className={classes.city}>
-        <Input name="city" type="text" label="City" register={register} error={null} />
+        <Input name="city" type="text" label="City" register={register} error={null} onChange={handleCity}/>
       </div>
       <div className={classes.postalCode}>
-        <Input name="postalcode" type="text" label="Postal Code" register={register} error={null} />
+        <Input name="postalcode" type="text" label="Postal Code" register={register} error={null} onChange={handlePostalCode}/>
       </div>
       <div className={classes.country}>
-        <Input name="country" type="text" label="Country" register={register} error={null} />
+        <Input name="country" type="text" label="Country" register={register} error={null} onChange={handleCountry}/>
       </div>
       <div className={classes.phone}>
-        <Input name="phone" type="text" label="Phone Number" register={register} error={null} />
+        <Input name="phone" type="text" label="Phone Number" register={register} error={null} onChange={handlePhone}/>
       </div>
       <div className={classes.email}>
-        <Input name="email" type="email" label="Email" register={register} error={null} />
+        <Input name="email" type="email" label="Email" register={register} error={null} onChange={handleEmail}/>
       </div>
     </div>
         <Button label="Back to cart" href="/cart" appearance="secondary" />
